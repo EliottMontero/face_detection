8dbcdc064607dec9e130f385d9b0a85225a432a7
emontero <eliott.montero@etu.u-bordeaux.fr>
2019-11-26 12:11:30 +0100

    face detection ok

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     filter2d = False
     gaussian = True
     median = False
-    
+
+    weigthPath = "opencvBasedDetector/detector/res10_300x300_ssd_iter_140000.caffemodel"
+    modelPath = "opencvBasedDetector/detector/deploy.prototxt.txt"
+
     cam = cv2.VideoCapture(0)
 
     fps = cam.get(cv2.CAP_PROP_FPS) # number of frames per second
@@ -17,13 +20,17 @@ if __name__ == '__main__':
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter('output.avi', fourcc, fps, (fw,fh))
 
+    thr = 0.2
     m = 3
     sigma = 1.5
     changingSigma = False
     changingM = False
+    net = cv2.dnn.readNetFromCaffe(modelPath, weigthPath)
     #cam = cv2.VideoCapture('videof.webm')
     while True:
         ret_val, img = cam.read()
+        h = img.shape[0]
+        w = img.shape[1]
         if mirror:
             img = cv2.flip(img, 1)
         if changingSigma:
@@ -51,6 +58,24 @@ if __name__ == '__main__':
 
         out.write(img)
         cv2.imshow("Filtered", img)
+
+        image = img
+        blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+        net.setInput(blob)
+        cvDet = net.forward()
+
+        detection = []
+        for i in range(0, cvDet.shape[2]): # Iterate over top-200 predicted boxes (defined on the output layer in deploy.prototxt file for caffe network)
+            confidence = cvDet[0, 0, i, 2] # For each box, check confidence
+            if confidence > thr: # If confidence is over detection_threshold we save the box
+                box = cvDet[0, 0, i, 3:7] * np.array([w, h, w, h]) # Get the bounding box from predictions array (see range 3:7 in cvDet[0,0,i,3:7]), returns 4 elements
+                (startX, startY, endX, endY) = box.astype("int") # get limits of the bounding box
+                text = "{:.2f}%".format(confidence * 100)
+                cv2.rectangle(image, (startX, startY), (endX, endY),(0, 0, 255), 2)
+                cv2.putText(image, text, (startX, startY),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                detection.append([ [startX,startY] , [endX,endY] ]) # Append bounding boxes with new format, a list of lists...
+
+        cv2.imshow("OUTPUT", image)
 
         k = cv2.waitKey(1)
         if k == 27:
